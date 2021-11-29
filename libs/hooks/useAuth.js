9ccb2a6870e83fe4eb41d887aaca84f384cf7626
Xiao Chen <cxcanada@hotmail.com>
2021-11-29 10:49:23 -0700

    updated google auth and github auth on Nov 29 class - code along

--- libs/hooks/useAuth.js
+++ libs/hooks/useAuth.js
@@ -4,16 +4,19 @@ import { auth } from 'libs/firebase'
 
 function useAuth() {
     const [user, setUser] = useState(null)
-
     useEffect(() => {
-        onAuthStateChanged(auth, (clientCredential) => {
-            if (clientCredential) {
-                setUser(clientCredential)
-            } else {
-                setUser(null)
-            }
-        })
-    })
+        // components mounts
+        const authChange = onAuthStateChanged(auth, (clientCredential) => {
+                if (clientCredential) {
+                    setUser(clientCredential)
+                } else {
+                    setUser(null)
+                }
+            })
+            // components unmount
+        return () => authChange()
+    }, [])
     return user
 }
+
 export { useAuth }
